d99111d7ae9cfc64ef1f9777dd1395ad742ec2db
manhee <angara99@gmail.com>
2024-01-12 11:39:55 -0800

    Added yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -78,7 +78,30 @@ fbq('track', 'PageView');`}
           width="1"
           style={{ display: 'none' }}
           src="https://www.facebook.com/tr?id=311940005157178&ev=PageView&noscript=1"
+          alt="fb"
         />
+      </noscript>
+
+      <Script id="yandex" type="text/javascript">
+        {`
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(96118020, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+  `}
+      </Script>
+      <noscript>
+        <div>
+          <img src="https://mc.yandex.ru/watch/96118020" style={{ position: 'absolute', left: -9999 }} alt="yandex" />
+        </div>
       </noscript>
 
       <ThemeProvider>

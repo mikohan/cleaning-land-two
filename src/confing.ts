cc59a59c38cfa6aea7121ec605e609eebefdda14
manhee <angara99@gmail.com>
2024-01-12 10:31:45 -0800

    Added privacy policy

--- src/confing.ts
+++ src/confing.ts
@@ -1,3 +1,4 @@
 export const phone = '+1 (213) 598-7763';
 export const email = 'angaralabllc@gmail.com';
 export const companyName = 'Playa Cleaning';
+export const site = 'https://playacleaning.com';
